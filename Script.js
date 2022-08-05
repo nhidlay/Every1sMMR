7b5e4755d9cc7870b046ba4fa9abf223fec304ca
nhidlay <95630868+nhidlay@users.noreply.github.com>
2022-08-05 10:23:15 -0600

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,12 +1,24 @@
-// JavaScript source code
+// calls printFunction when button is clicked
 	
 	document.getElementById("myButton").addEventListener("click", printFunction);
 
+// Takes search box value and puts summoner names in an array, then prints them to screen
 	function printFunction()
 	{
 		let searchInput = document.getElementById("mySearch").value; 
 		
-		document.getElementById("searchOutput").innerHTML = searchInput;
+		let summonerNames = searchInput.replace(/joined the lobby/g,"");
+		
+		let summonerName = summonerNames.split("  ");
+		
+		
+		
+		for (let i = 0; i < summonerName.length; i++)
+		{
+			let para = document.createElement("p");
+			para.innerText = summonerName[i];
+			document.body.appendChild(para);
+		}
 	}
 
 	
